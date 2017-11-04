db7de8e4e4af05e664cc379e614984eff329eab8
Chen Jin <chjjin@cs.stonybrook.edu>
2017-11-04 02:14:56 -0400

    part3

--- hw4/src/sfish.c
+++ hw4/src/sfish.c
@@ -4,25 +4,41 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include "debug.h"
+
 
 
 
 #define MAXARGS 128
 int argc;
 char bufArray[1024];
+char *new_prompt;
+char previous_direc[1024];
+
 int parseLine(char *buf, char **argv);
 int builtin_command(char** argv);
 void help_print();
 void self_pwd(char* bufArray);
 void self_cd(char** argv);
-char *new_prompt;
-char previous_direc[1024];
 void Executables(char* input,char** argv);
 char* getpath(char* argv);
 int slashexit(char* first_argv);
 int check_execut(char* first_argv);
+int redirection_exist(char* input);
+void Redirection(char* input,char** argv);
+char check_direction_char(char* input);
+char** parse_redirection_argument(char* input,char** directionList,char* parameter );
+char* getfile_path(char* path);
+int check_buildin_commond(char* execute_path);
+int check_less_brefore_greater(char* input);
+int check_greater_brefore_less(char* input);
+void execute_pipe(int in,int out,char** first_token_list);
+void handle_pipe(int array_size, char** directionList);
+
 
 int eval(char* input,char* prompt){
+    // previous_direc[0] = '\0';
     char *argv[MAXARGS];
     char* buf = strdup(input);
     new_prompt = prompt;
@@ -33,7 +49,9 @@ int eval(char* input,char* prompt){
     else if (strcmp(argv[0],"exit") == 0){
         return -1;
     }
-
+    else if (redirection_exist(input)){
+        Redirection(input,argv);
+    }
     else if(builtin_command(argv)!=1){
         Executables(input,argv);
     }
@@ -41,10 +59,506 @@ int eval(char* input,char* prompt){
     return 0;
 }
 
+
+void Redirection(char* input,char** argv){
+
+
+    if(check_less_brefore_greater(input) == 1){
+
+        debug("%s\n", "it is rerally ok mow dude");
+        char** directionList = malloc(sizeof(char*));
+        char* parameter = "<";
+        char* space = " ";
+        char* greater = ">";
+        directionList=parse_redirection_argument(input,directionList,parameter);
+        char* first_token = directionList[0];
+        char* second_token  = directionList[1];
+        debug("this is first token:%s\n", first_token);
+        debug("this is second token:%s\n", second_token);
+
+        char** first_token_list = malloc(sizeof(char*));
+        first_token_list = parse_redirection_argument(first_token,first_token_list,space);
+
+        char** second_token_list = malloc(sizeof(char*));
+        second_token_list = parse_redirection_argument(second_token,second_token_list,greater);
+
+        char** second_argv = malloc(sizeof(char*));
+        second_argv = parse_redirection_argument(second_token_list[0],second_argv,space);
+
+        char** third_argv = malloc(sizeof(char*));
+        third_argv = parse_redirection_argument(second_token_list[1],third_argv,space);
+        debug("this is first token:%s\n", first_token);
+        debug("this is second argv token:%s\n", second_argv[0]);
+        debug("this is third argv token:%s\n", third_argv[0]);
+
+
+
+        char* input_path = getfile_path(second_argv[0]);
+        debug("this is first_token_list[0]:%s\n", first_token_list[0]);
+
+        int inputfile = open(input_path,O_RDONLY);
+
+        char* output_path = getfile_path(third_argv[0]);
+
+        int outputfile = open(output_path,O_WRONLY|O_CREAT|O_TRUNC);
+
+
+        if(inputfile == -1 ){
+            printf(EXEC_ERROR, "error when open inputfile");
+        }
+        if(outputfile ==-1){
+            printf(EXEC_ERROR, "error when open outputfile");
+        }
+
+
+        int status;
+        pid_t pid;
+
+        if((pid = fork()) == 0){
+            dup2(inputfile,STDIN_FILENO);
+            dup2(outputfile, STDOUT_FILENO);
+            close(inputfile);
+            close(outputfile);
+            // printf("%s\n", "you can never be here");
+            // printf("this is :%s\n", first_token_list[0]);
+            // printf("first:%s\n", first_token_list[2]);
+
+                char* execute_path = getpath(first_token_list[0]);
+                int execv_return = execv(execute_path,first_token_list);
+                if(execv_return == -1){
+                    printf(EXEC_ERROR,"cannot execute! " );
+                    exit(0);
+                }
+
+        }
+        else{
+            wait(&status);
+        }
+        free(directionList);
+        free(first_token_list);
+        free(second_token_list);
+        free(second_argv);
+        free(third_argv);
+
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////
+    else if(check_greater_brefore_less(input) == 1){
+
+        debug("%s\n", "it is rerally ok mow dude");
+        char** directionList = malloc(sizeof(char*));
+        char* parameter = ">";
+        char* space = " ";
+        char* greater = "<";
+        directionList=parse_redirection_argument(input,directionList,parameter);
+        char* first_token = directionList[0];
+        char* second_token  = directionList[1];
+        debug("this is first token:%s\n", first_token);
+        debug("this is second token:%s\n", second_token);
+
+        char** first_token_list = malloc(sizeof(char*));
+        first_token_list = parse_redirection_argument(first_token,first_token_list,space);
+
+        char** second_token_list = malloc(sizeof(char*));
+        second_token_list = parse_redirection_argument(second_token,second_token_list,greater);
+
+        char** second_argv = malloc(sizeof(char*));
+        second_argv = parse_redirection_argument(second_token_list[0],second_argv,space);
+
+        char** third_argv = malloc(sizeof(char*));
+        third_argv = parse_redirection_argument(second_token_list[1],third_argv,space);
+        debug("this is first token:%s\n", first_token);
+        debug("this is second argv token:%s\n", second_argv[0]);
+        debug("this is third argv token:%s\n", third_argv[0]);
+
+
+
+        char* input_path = getfile_path(third_argv[0]);
+        debug("this is first_token_list[0]:%s\n", first_token_list[0]);
+
+        int inputfile = open(input_path,O_RDONLY);
+
+        char* output_path = getfile_path(second_argv[0]);
+
+        int outputfile = open(output_path,O_WRONLY|O_CREAT|O_TRUNC);
+
+
+        if(inputfile == -1 ){
+            printf(EXEC_ERROR, "error when open inputfile");
+        }
+        if(outputfile ==-1){
+            printf(EXEC_ERROR, "error when open outputfile");
+        }
+
+
+        int status;
+        pid_t pid;
+
+        if((pid = fork()) == 0){
+            dup2(inputfile,STDIN_FILENO);
+            dup2(outputfile, STDOUT_FILENO);
+            close(inputfile);
+            close(outputfile);
+            // printf("%s\n", "you can never be here");
+            // printf("this is :%s\n", first_token_list[0]);
+            // printf("first:%s\n", first_token_list[2]);
+
+                char* execute_path = getpath(first_token_list[0]);
+                int execv_return = execv(execute_path,first_token_list);
+                if(execv_return == -1){
+                    printf(EXEC_ERROR,"cannot execute! " );
+                    exit(0);
+                }
+
+        }
+        else{
+            wait(&status);
+        }
+        free(directionList);
+        free(first_token_list);
+        free(second_token_list);
+        free(second_argv);
+        free(third_argv);
+
+    }
+    ///////////////////////////////////////////////////////////////////////////////
+    else if(check_direction_char(input) == '>'){
+        char** directionList = malloc(sizeof(char*));
+        char* parameter = ">";
+        char* space = " ";
+        directionList=parse_redirection_argument(input,directionList,parameter);
+        char* first_token = directionList[0];
+        char* second_token  = directionList[1];
+        // free(directionList);
+        debug("this is first token:%s\n", first_token);
+        debug("this is second token:%s\n", second_token);
+
+        char** first_token_list = malloc(sizeof(char*));
+        first_token_list = parse_redirection_argument(first_token,first_token_list,space);
+
+        char** second_token_list = malloc(sizeof(char*));
+        second_token_list = parse_redirection_argument(second_token,second_token_list,space);
+
+        char* output_path = getfile_path(second_token_list[0]);
+        debug("this is out put path:%s\n", output_path);
+        debug("this is first_token_list[0]:%s\n", first_token_list[0]);
+
+        int file = open(output_path,O_WRONLY|O_CREAT|O_TRUNC);
+        if(file == -1){
+            printf(EXEC_ERROR, "error when open file");
+        }
+        int status;
+        pid_t pid;
+
+        if((pid = fork()) == 0){
+            dup2(file,STDOUT_FILENO);
+            close(file);
+            // int build_Comand = check_buildin_commond(first_token_list[0]);
+            if(builtin_command(first_token_list) == 1){
+                exit(1);
+            }
+            else{
+                char* execute_path = getpath(first_token_list[0]);
+                int execv_return = execv(execute_path,first_token_list);
+                if(execv_return == -1){
+                    printf(EXEC_ERROR,"cannot execute! " );
+                    exit(0);
+                }
+            }
+        }
+        else{
+            wait(&status);
+
+        }
+        free(directionList);
+        free(first_token_list);
+        free(second_token_list);
+
+    }
+
+/////////////////////////////////////////////////////////////////////////////////
+    else if(check_direction_char(input) == '<'){
+        char** directionList = malloc(sizeof(char*));
+        char* parameter = "<";
+        char* space = " ";
+        directionList=parse_redirection_argument(input,directionList,parameter);
+        char* first_token = directionList[0];
+        char* second_token  = directionList[1];
+        // free(directionList);
+        debug("this is first token:%s\n", first_token);
+        debug("this is second token:%s\n", second_token);
+
+        char** first_token_list = malloc(sizeof(char*));
+        first_token_list = parse_redirection_argument(first_token,first_token_list,space);
+
+        char** second_token_list = malloc(sizeof(char*));
+        second_token_list = parse_redirection_argument(second_token,second_token_list,space);
+
+        char* input_path = getfile_path(second_token_list[0]);
+        debug("this is in put path:%s\n", input_path);
+        debug("this is first_token_list[0]:%s\n", first_token_list[0]);
+
+        int file = open(input_path,O_RDONLY);
+        if(file == -1){
+            printf(EXEC_ERROR, "file not exit");
+        }
+
+        int status;
+        pid_t pid;
+
+        if((pid = fork()) == 0){
+            dup2(file,STDIN_FILENO);
+            close(file);
+            // int build_Comand = check_buildin_commond(first_token_list[0]);
+
+                char* execute_path = getpath(first_token_list[0]);
+                int execv_return = execv(execute_path,first_token_list);
+                if(execv_return == -1){
+                    printf(EXEC_ERROR,"cannot execute! " );
+                    exit(0);
+                }
+        }
+        else{
+            wait(&status);
+
+        }
+        free(directionList);
+        free(first_token_list);
+        free(second_token_list);
+
+    }
+
+//////////////////////////////////////////////////////////////////////////////////
+    if(check_direction_char(input) == '|'){
+        int status;
+        pid_t pid;
+        char** directionList = malloc(sizeof(char*));
+        char* parameter = "|";
+        // char* space = " ";
+        directionList=parse_redirection_argument(input,directionList,parameter);
+
+        int array_size  = 0;
+        while(directionList[array_size]!= NULL){
+            array_size++;
+        }
+        debug("array size:%i\n",array_size );
+
+        if((pid = fork()) == 0){
+                // if(builtin_command(first_token_list) == 1){
+                //     exit(1);
+                    // }
+
+
+
+                handle_pipe(array_size,directionList);
+                exit(0);
+
+            }
+
+
+        else{
+            wait(&status);
+
+        }
+        free(directionList);
+
+
+        }
+
+}
+
+void handle_pipe(int array_size, char** directionList){
+    char* space = " ";
+    int fd[2];
+
+
+    int in_file = 0 ;
+    int out_file = 0;
+    for (int i = 0;i<array_size-1;i++){
+        char* first_token = directionList[i];
+
+        char** first_token_list = malloc(sizeof(char*));
+        first_token_list = parse_redirection_argument(first_token,first_token_list,space);
+        debug("first_token_list:%s\n", first_token_list[0]);
+        pipe(fd);
+        out_file = fd[1];
+        execute_pipe(in_file,out_file,first_token_list);
+        close(fd[1]);
+        in_file = fd[0];
+    }
+    if(in_file!=STDIN_FILENO){
+        dup2(in_file,STDIN_FILENO);
+        close(in_file);
+        char* first_token = directionList[array_size-1];
+
+        char** first_token_list = malloc(sizeof(char*));
+        first_token_list = parse_redirection_argument(first_token,first_token_list,space);
+        char* execute_path = getpath(first_token_list[0]);
+        execv(execute_path,first_token_list);
+    }
+
+}
+
+void execute_pipe(int in_file,int out_file,char** first_token_list){
+    // int status;
+    pid_t pid;
+    char* execute_path;
+    if((pid = fork()) == 0){
+        if(in_file!=STDIN_FILENO){
+          dup2(in_file,STDIN_FILENO);
+          close(in_file);
+        }
+        if(out_file!= STDOUT_FILENO){
+            dup2(out_file,STDOUT_FILENO);
+            close(out_file);
+        }
+
+        // if(builtin_command(first_token_list) == 1){
+        //     // if (strcmp(first_token_list[0], "help") == 0) {
+        //     //     help_print();
+        //     // }
+
+        //     // if(!strcmp(first_token_list[0], "pwd")){
+        //     //     self_pwd(bufArray);
+        //     //     printf("%s\n", bufArray);
+        //     // }
+        // }
+
+        // else{
+            execute_path = getpath(first_token_list[0]);
+            int execv_return = execv(execute_path,first_token_list);
+            if(execv_return == -1){
+                printf(EXEC_ERROR,"cannot execute! " );
+                exit(0);
+            // }
+        }
+
+
+    }
+
+
+}
+
+
+
+int check_greater_brefore_less(char* input){
+    int length = strlen(input);
+    int greater = 0;
+    int less = 0;
+    for(int i =0;i<length;i++){
+        if(*(input+i) == '>' ){
+            greater = i;
+        }
+        else if(*(input+i) == '<'){
+            less  = i;
+        }
+    }
+
+    if(greater<less  && less !=0 && greater!=0){
+        return 1;
+    }
+    return 0;
+}
+
+
+int check_less_brefore_greater(char* input){
+    int length = strlen(input);
+    int greater = 0;
+    int less = 0;
+    for(int i =0;i<length;i++){
+        if(*(input+i) == '>' ){
+            greater = i;
+        }
+        else if(*(input+i) == '<'){
+            less  = i;
+        }
+    }
+
+    if(less<greater  && less !=0 && greater!=0){
+        return 1;
+    }
+    return 0;
+}
+
+int check_buildin_commond(char* execute_path){
+    char help[5];
+    strcpy(help,"help");
+    char pwd[4];
+    strcpy(pwd,"pwd");
+    if(strcmp(execute_path,help) == 0){
+        return 1;
+    }
+    if(strcmp(execute_path,pwd) == 0){
+        return 1;
+    }
+    return 0;
+}
+char* getfile_path(char* path){
+    int flag = 0;
+    int length = strlen(path);
+    char* filepath = NULL;
+    for (int i =0;i<length;i++){
+        if(*(path+i) == '/'){
+            flag = 1;
+        }
+    }
+    if(flag == 0){
+        self_pwd(bufArray);
+        filepath = bufArray;
+        filepath = strcat(filepath,"/");
+        filepath = strcat(filepath,path);
+    }
+    return filepath;
+}
+
+char** parse_redirection_argument(char* input,char** directionList,char* parameter ){
+    char* token;
+    int total_size = 0;
+
+    token = strtok(input, parameter);
+    while(token!= NULL){
+        total_size = total_size+1;
+        directionList = realloc(directionList,total_size*sizeof(char*));
+        directionList[total_size-1] = token;
+
+        token = strtok(NULL, parameter);
+    }
+
+    directionList[total_size] = 0;      //null pointer
+    return directionList;
+
+}
+
+char check_direction_char(char* input){
+    int length = strlen(input);
+    for(int i =0;i<length;i++){
+        if(*(input+i) == '>' ){
+            return '>';
+        }
+        else if(*(input+i) == '<'){
+            return '<';
+        }
+        else if(*(input+i) == '|'){
+            return '|';
+        }
+    }
+    return 0;
+}
+int redirection_exist(char* input){
+    int length = strlen(input);
+    for(int i =0;i<length;i++){
+        if(*(input+i) == '>' || *(input+i) == '<'  || *(input+i) == '|'){
+            return 1;
+        }
+    }
+    return 0;
+}
+
 void Executables(char* input,char** argv){
     pid_t pid;
     char* path = strdup(argv[0]);
-    int status;
+    int status ;
 
     if(slashexit(argv[0]) == 1){
         if(check_execut(argv[0]) == 0){
@@ -74,9 +588,6 @@ void Executables(char* input,char** argv){
     else{
         wait(&status);
     }
-
-
-
 }
 
 int check_execut(char* first_argv){
@@ -214,7 +725,7 @@ void self_cd(char** argv){
 
     if(strcmp(path,"-") == 0){
         if(previous_direc == NULL){
-            fprintf(stderr,"No previous directory \n");
+            printf(BUILTIN_ERROR,"No previous directory");
         }
         else{
             self_pwd(bufArray);
@@ -226,7 +737,7 @@ void self_cd(char** argv){
         self_pwd(bufArray);
         strcpy(previous_direc,bufArray);
         if ((chdir(path)) == -1){
-            fprintf(stderr,"No such file or directory \n");
+            printf(BUILTIN_ERROR,"No such file or directory");
         }
 
     }
